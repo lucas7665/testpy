c7466acde681ddc5a946f61d9644f7ca3fd0e230
Lucas <lucas@example.com>
2025-09-16 16:08:44 +0800

    Initial commit: 添加README、Python程序和测试文件

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Hello World 程序
+用于测试Git版本控制
+"""
+
+def hello_world():
+    """打印Hello World消息"""
+    print("Hello, World!")
+    print("这是一个测试Gitea和IDEA集成的Python程序")
+
+if __name__ == "__main__":
+    hello_world()
